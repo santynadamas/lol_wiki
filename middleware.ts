90d9aafe04125a95f3abfcc0f3e1f0997a74b7de
santynadamas <santiagorojass018@gmail.com>
2026-06-16 18:17:16 -0300

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
-import NextAuth from "next-auth"
-import { authConfig } from "@/auth.config"
-
-export const { auth: middleware } = NextAuth(authConfig)
+// middleware.ts
+export { auth as middleware } from "@/auth"
 
 export const config = {
-  // Incluye /login para que el callback pueda redirigir usuarios ya autenticados
   matcher: ["/profile/:path*", "/login"],
 }
